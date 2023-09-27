8154b5d1abbc31f54f4b21917197c40516799ecd
yasirqureshi <sir.qurashi@gmail.com>
2023-09-27 16:40:47 +0500

    work Assignment 3

---
+++ app.js
@@ -0,0 +1,32 @@
+// declare variable age then assign and alert
+var age;
+age="41";
+
+alert("I am " + age + " years old")
+
+// declare and initailize visits in variable
+
+var N =14;
+
+var messageFirstHalf= "you have visited this site ";
+var messageSecondtHalf=" times"
+
+alert(messageFirstHalf+N+messageSecondtHalf);
+
+//birth year in browser
+
+var birthYear = 1984;
+var textBefore = "My Birth Year is";
+var textAfter = "<h3>My type of declared variable is number</h3>";
+var work = "<h1>Web & App Yasir Rasheed CCO - 118488</h1>"
+var call = "Contact Me 03158892047";
+
+document.write(textBefore+birthYear+"<br>"+textAfter+"<br>"+work+"<br>"+call);
+
+//script for store visitors order details
+
+var userName = "<b>Yasir Qureshi</b> ";
+var productQuantity = 8;
+var productTitle = "<b> Shirts</b>";
+
+document.write("<br><br><br>"+userName+"orderd "+"<b>"+productQuantity+"</b>"+productTitle+" on XYZ Clothing Store");
